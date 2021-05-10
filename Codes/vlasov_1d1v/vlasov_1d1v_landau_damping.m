c571958cf002ce134e80cade14022f0021389972
tiegcm <63451870+tiegcm@users.noreply.github.com>
2021-05-10 20:21:29 +0800

    Update vlasov_1d1v_landau_damping.m

--- Codes/vlasov_1d1v/vlasov_1d1v_landau_damping.m
+++ Codes/vlasov_1d1v/vlasov_1d1v_landau_damping.m
@@ -6,7 +6,7 @@ NO2 = NO/2; % # of ghost cells on each side
 
 % 1. generate a 2-D cartesian grid
 nx = 64; % # of active cells in x space
-ny = 64; % # of active cells in v space
+ny = 128; % # of active cells in v space
 
 L = 2*pi; % simulation domain from 0 to L
 Vmax = 6; % velocity space from -Vmax to Vmax
